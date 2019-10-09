67addf5505b1d4270f327cb3a619fab42fdad044
Quoc Tran <stevelee2497@gmail.com>
2019-10-09 18:02:48 +0700

    Add File Upload API Service

---
+++ API/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Services.DTOs.Output;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/files")]
+    public class FileController : ControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public FileController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        public Task<BaseResponse<string>> Upload(IFormFile file, [FromForm] string category)
+        {
+            return _fileService.Upload(file, category);
+        }
+
+        [HttpGet("{category}/{fileName}")]
+        public Task<FileStreamResult> Download(string category, string fileName)
+        {
+            return _fileService.Download($"{category}/{fileName}");
+        }
+    }
+}
---
+++ Services/Abstractions/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.DTOs.Output;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public interface IFileService
+    {
+        Task<BaseResponse<string>> Upload(IFormFile file, string category);
+        Task<FileStreamResult> Download(string fileRelativePath);
+    }
+}
