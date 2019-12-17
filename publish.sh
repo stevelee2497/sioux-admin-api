eb2885d143ba36f15ce588ccbd3d6fa426c61b75
Steven <stevelee2497@gmail.com>
2019-12-17 21:06:35 +0700

    update sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,13 @@
 CURRENT_PATH=$(pwd)
+BUILD_FOLDER=build-api
 OUTPUT_PATH="${CURRENT_PATH}/build-api"
+REMOTE_ID=ubuntu@52.187.169.33
+DEPLOYMENT_REMOTE_PATH='~/sioux-admin-api'
+CONTAINER=sioux-admin-api
 
 echo "Cleaning ..."
-rm -rf build-api
-mkdir -p build-api
+rm -rf ${BUILD_FOLDER}
+mkdir -p ${BUILD_FOLDER}
 
 echo "Building API packages ..."
 cd API
@@ -11,6 +15,17 @@ dotnet publish API.csproj -o "${OUTPUT_PATH}"
 
 cd "${CURRENT_PATH}"
 
-echo "Send packages to server"
-scp -C -r 'build-api' ssh ubuntu@52.187.169.33:~/sioux-admin-api
-ssh ssh ubuntu@52.187.169.33 "cd ~/sioux-admin-api;docker-compose up -d --build sioux-admin-api"
+read -r -p "Sending build to server and restart service now? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        echo "SENDING PACKAGE ..."
+        scp -C -P 22 -r ${BUILD_FOLDER} ${REMOTE_ID}:${DEPLOYMENT_REMOTE_PATH}
+
+        echo "RESTART DOCKER ..."
+        export REMOTE_DOCKER_COMMAND="cd ${DEPLOYMENT_REMOTE_PATH};docker-compose up -d --force-recreate ${CONTAINER};"
+        ssh -p 22 ${REMOTE_ID} ${REMOTE_DOCKER_COMMAND}
+        ;;
+    *)
+        echo "not sending data, service not restarted. Exiting ..."
+        ;;
+esac
